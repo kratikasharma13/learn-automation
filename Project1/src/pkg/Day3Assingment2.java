ad1771e0ef8a994e54d5f911e6444b9b5ba048fa
Dhruv Sharma1 <Dhruv Sharma1@DESKTOP-5DS41OC>
2022-08-12 22:16:53 +0530

    12th august commit

--- Project1/src/pkg/Day3Assingment2.java
+++ Project1/src/pkg/Day3Assingment2.java
@@ -5,38 +5,33 @@ public class Day3Assingment2 {
 	public void a() {
 		this.c(1, 2, 3);
 		System.out.println("default method");	
-		
-	}
+					 }
 	
 	public void a1(int a) {
 		this.d(5, 6, 7, 8);
-		System.out.println("1 parameterized method");	
-		
-	}
+		System.out.println("1 parameterized method");		
+						  }
 	
 	public void b(int a, int b) {
 		this.a1(4);
 		System.out.println("2 parameterized method");	
-		
-	}
+								}
 	
 	public void c(int a, int b, int c) {
 		System.out.println("3 parameterized method");	
-		
-	}
+										}
 	
 	public void d(int a, int b, int c, int d) {
 		this.a();
 		System.out.println("4 parameterized method");	
-		
-	}
+											  }
 	
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Day3Assingment2 obj1= new Day3Assingment2();
 		obj1.b(2,3);
+											}
 
-	}
 
-}
+								}
